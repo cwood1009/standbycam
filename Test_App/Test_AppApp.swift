be07039d18b43e432b3eaa72a47c1b2808a83f03
Training1 <cwood@anbank.com>
2025-09-02 12:05:07 -0500

    Initial Commit

---
+++ Test_App/Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_AppApp.swift
+//  Test_App
+//
+//  Created by Chris Wood on 9/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Test_AppTests/Test_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Test_AppTests.swift
+//  Test_AppTests
+//
+//  Created by Chris Wood on 9/2/25.
+//
+
+import Testing
+@testable import Test_App
+
+struct Test_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
